2c7a1caaab4d30a2b806f5e73ad17d6622f0beb1
Alex Renteria <alexburkerenteria@gmail.com>
2026-03-28 13:21:49 -0400

    set up flutter app

---
+++ vacavetion/README.md
@@ -0,0 +1,3 @@
+# vacavetion
+
+A new Flutter project.
---
+++ vacavetion/android/app/src/main/kotlin/com/example/vacavetion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vacavetion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
